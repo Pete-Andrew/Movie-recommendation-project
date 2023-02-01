a067420e8d1ef4fe312f5e768acafbf8cbbfc02a
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2023-02-01 19:44:47 +0000

    added ajax function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ var APIkey = "67a9b854";
 var submitButton = document.getElementById("submit-button");
 
 // http://www.omdbapi.com/?apikey=[yourkey]&
+http://img.omdbapi.com/?apikey=[yourkey]&
+
+
 
 submitButton.onclick = function (event) {
     event.preventDefault(); 
@@ -9,12 +12,18 @@ submitButton.onclick = function (event) {
     var filmTitle = $("#search").val() 
     console.log(filmTitle);
 
-    var filmInfo = "http://www.omdbapi.com/?apikey=" + APIkey + "&t=" + filmTitle ; 
+    var filmInfo = "http://www.omdbapi.com/?apikey=" + APIkey + "&t=" + filmTitle; 
     console.log(filmInfo); 
 
+    $.ajax({
+        url: filmInfo,
+        method: "GET", 
+        }).then(function (filmInfoFromAPI) {
+            console.log(filmInfoFromAPI);
+
+        }); 
+    
 }
 
 
 
-
-// http://www.omdbapi.com/?apikey=[yourkey]&
