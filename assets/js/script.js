b78083a53963bb2c8990d8728b7d9d638dd6e6c8
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2023-02-02 20:41:47 +0000

    film details now appear in the jumbotron

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,14 @@ $("#time").text(time.format("DD-MM-YYYY"));
 var APIkey = "67a9b854";
 var submitButton = document.getElementById("submit-button");
 
+var actors = $("#actors");
+var awards = $("#awards");
+var rated = $("#rated");
+var director = $("director");
+var IMDBrating = $("imdb-rating");
+var released = $("released");
+
+
 //creates an onclick function that takes the input film name, replaces the white space in it with +, and pulls the film info from the API
 submitButton.onclick = function (event) {
     event.preventDefault(); 
@@ -26,6 +34,13 @@ submitButton.onclick = function (event) {
 
             var poster = $("#poster1");
             poster.attr("src", APIResponse.Poster);
+ 
+            $("#actors").text("Actors: "   + APIResponse.Actors); 
+            $("#awards").text("Awarrds:" + APIResponse.Awards); 
+            $("#rated").text("Rated: " + APIResponse.Rated); 
+            $("#director").text("Director: " + APIResponse.Director); 
+            $("#imdb-rating").text("IMDB rating: " + APIResponse.Rating);
+            $("#released").text("Release Date: " + APIResponse.Released);           
            
         }); 
    
